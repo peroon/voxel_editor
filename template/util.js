979e5deeab155d2cc20812f5d0dd085195de7954
Keita Ishibashi <fromjpn@gmail.com>
2013-08-13 22:54:08 +0900

    textured cube

--- template/util.js
+++ template/util.js
@@ -3,22 +3,19 @@ function p(v){
 }
 
 function getImageCube(){
-	// var materials = [
-	// 	new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture("./me_avater.jpg")}),
-	// 	new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture("./me_avater.jpg")}),
-	// 	new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture("./me_avater.jpg")}),
-	// 	new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture("./me_avater.jpg")}),
-	// 	new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture("./me_avater.jpg")}),
-	// 	new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture("./me_avater.jpg")})
-	// ];
-	// var material = new THREE.MeshFaceMaterial();
-	// material = new THREE.MeshLambertMaterial( { color: 0xdddddd, wireframe: true } );
-	// var geometry = new THREE.CubeGeometry(1,1,1,null,null,null,materials);
-	// var mesh = new THREE.Mesh( geometry, material );
-	// return mesh;
+	var path = './me_avater.jpg';
+	var materials = [
+       new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture(path)}),
+       new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture(path)}),
+       new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture(path)}),
+       new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture(path)}),
+       new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture(path)}),
+       new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture(path)}),
+    ];
 
-	var geometry = new THREE.CubeGeometry(1,1,1);
-	var material = new THREE.MeshBasicMaterial({color: 0x990000});
-	var cube = new THREE.Mesh(geometry, material);
-	return cube;
+	var dice = new THREE.Mesh(
+	    new THREE.CubeGeometry(2, 2, 2, 1, 1, 1),
+	    new THREE.MeshFaceMaterial(materials)
+	    );
+	return dice;
 }
