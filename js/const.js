06206265aea17f36c9dd2165111f9c8991d7367e
Keita Ishibashi <fromjpn@gmail.com>
2013-08-30 02:38:31 +0900

    const

---
+++ js/const.js
@@ -0,0 +1,8 @@
+//アローキー
+var KEY_LEFT  = 37;
+var KEY_UP    = 38;
+var KEY_RIGHT = 39;
+var KEY_DOWN  = 40;
+
+//キューブサイズ
+var CUBE_WIDTH = 50;
--- js/util.js
+++ js/util.js
@@ -32,3 +32,31 @@ function getLine(v0, v1, color){
 	var line = new THREE.Line(lineGeometry, lineMaterial);
 	return line;
 }
+
+function getStats(){
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	return stats;
+}
+
+//角度を範囲内に
+function degreeInRange(degree){
+	if(degree < 0){
+		degree += 360;
+	}
+	else if(degree > 360){
+		degree -= 360;
+	}
+	return degree;
+}
+//thetaを1-179 degreeに
+function thetaInRange(theta){
+	if(theta < 1){
+		theta = 1;
+	}
+	else if(theta > 179){
+		theta = 179;
+	}
+	return theta;
+}
