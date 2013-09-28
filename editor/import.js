8fca91b931cdd815959bd83ef5f7a976788126e5
Keita Ishibashi <fromjpn@gmail.com>
2013-09-29 02:07:44 +0900

    Extract x,y,z,color from JSON

--- editor/import.js
+++ editor/import.js
@@ -1,31 +1,62 @@
-function importFromWeb(){
-	p('import from web');
+function importFromWeb(id){
 
+	id = 6
 	//ajax GET
 	$.ajax({
-	    url: 'http://192.168.33.10:3000/voxeljsons/6',
+	    url: 'http://192.168.33.10:3000/voxeljsons/'+id,
 	    //url: 'http://api.stackoverflow.com/1.1/stats',
 	    type: 'GET',
 	    dataType: 'jsonp',
 	    jsonp : 'jsonp',
 
-	    success: function(html){
-	    	p('success html');
-	    	p(html);
-	    },
+	    // success: function(html){
+	    // 	p('success html');
+	    // 	p(html);
+	    // },
 
-	    error: function(html){
-	    	p('error html');
-	    	p(html);
-	    }
+	    // error: function(html){
+	    // 	p('error html');
+	    // 	p(html);
+	    // }
 	});
+}
 
-	p('imported from web');
+function callbackfunc(json_object){
+	//var json_str = JSON.stringify(json_object);
+
+	//JSONからボクセル生成
+	constructVoxels(json_object);
+}
+
+//JSONからキューブを配置する
+function constructVoxels(json_object){
+	var cubes = json_object.cubes;
+
+	//キー(色)でfor
+	var keys = Object.keys(cubes);
+
+	for(var i=0; i<keys.length; i++){
+		var color = keys[i];
+
+		//座標でfor
+		var xyzArray = cubes[color];
+
+		for(var j=0; j<xyzArray.length; j++){
+			var xyz = xyzArray[j];
+
+			var x = xyz.x;
+			var y = xyz.y;
+			var z = xyz.z;
+
+			//生成
+			generateVoxel(x,y,z,color);
+		}
+	}
 
 }
 
-function callbackfunc(v){
-	console.log(v);
-	console.log(v);
-	console.log(v);
+//ボクセル作成
+function generateVoxel(x,y,z,color){
+	p('gen voxel at');
+	p([x,y,z,color]);
 }
